d6faf6927331af5ecd93bdc8c2e104fd93b85fc4
Alvin Tan <vintan777@gmail.com>
2021-02-17 04:14:06 +0700

    second commit

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,14 +1,14 @@
 @extends('layout.master')
 
 @section('body')
-<div class="article-list-container">
+<div class="article-list-container mt-5 pt-5 ms-5">
     <div class="row">
         @foreach ($articles as $article)
             <div class="col-md-3">
                 <a href="{{ route('get_article', ['id'=>$article->id]) }}" class="text-dark"
                     style="text-decoration: none;">
                     <div class="card" style="width: 18rem;">
-                        <img src="{{asset($article->img_path)}}" class="card-img-top" alt="...">
+                        <img src="{{asset($article->image)}}" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="text-decoration-none card-title">
                                 {{$article->title}}
